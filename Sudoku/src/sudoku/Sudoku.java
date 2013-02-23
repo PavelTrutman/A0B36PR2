3e928ef512fd9bb2ce235e5cf03e4deed19084c0
Pavel <Pavel@192.168.1.5>
2013-02-23 10:47:44 +0100

    New empty project.

---
+++ Sudoku/src/sudoku/Sudoku.java
@@ -0,0 +1,14 @@
+package sudoku;
+
+/**
+ * @author Pavel Trutman
+ */
+public class Sudoku {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
