0f782ea4ce1eea2d357e1b678014e7a2ee28ede8
trutmpav <>
2013-03-16 11:02:01 +0100

    New main class.

---
+++ Sudoku/src/sudoku/Main.java
@@ -0,0 +1,19 @@
+package sudoku;
+
+/**
+ * @author Pavel Trutman
+ */
+public class Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+
+    GUI window;
+
+    window = new GUI();
+
+
+  }
+}
